d12d9bf838f62cec0138b493a99b4936d68bb209
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-12-23 15:07:53 +0300

    Добавьте файлы проекта.

---
+++ GameStaticsVolleyballMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStaticsVolleyballMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GameStaticsVolleyballMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStaticsVolleyballMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
